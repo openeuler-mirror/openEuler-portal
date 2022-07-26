beb538f5ff91a1c7586625845109fb1304d8a9fd
Bang <15622356989@163.com>
2022-07-26 16:19:10 +0800

    fix: 删除local缓存

--- app/.vitepress/src/components/AppTheme.vue
+++ app/.vitepress/src/components/AppTheme.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { computed, ref, onMounted, watch } from 'vue';
+import { computed, ref } from 'vue';
 import { useCommon } from '@/stores/common';
 
 import IconMoonLight from '~icons/app/sun.svg';
@@ -16,54 +16,13 @@ defineProps({
 const commonStore = useCommon();
 // const { t } = useI18n();
 
-const changeTheme = () => {
-  const theme = commonStore.theme === 'dark' ? 'light' : 'dark';
-  commonStore.theme = theme;
-  localStorage.setItem('euler-theme', theme);
-};
-
 const isLight = computed(() => (commonStore.theme === 'light' ? true : false));
 const mobileTheme = ref(!isLight.value);
-
-watch(
-  () => {
-    return commonStore.theme;
-  },
-  (val) => {
-    const documentElement = document.documentElement;
-    if (val === 'dark') {
-      documentElement.classList.remove('light');
-    } else if (val === 'light') {
-      documentElement.classList.remove('dark');
-    }
-    documentElement.classList.add(val);
-    localStorage.setItem('euler-theme', val);
-  }
-);
-
-onMounted(() => {
-  if (typeof global === 'undefined') {
-    const theme = localStorage.getItem('euler-theme');
-    if (theme) {
-      commonStore.theme = theme;
-      document.documentElement.classList.add(theme);
-    } else {
-      const isDark =
-        window.matchMedia &&
-        window.matchMedia('(prefers-color-scheme: dark)').matches;
-
-      const theme = isDark ? 'dark' : 'light';
-      commonStore.theme = theme;
-      document.documentElement.classList.add(theme);
-      localStorage.setItem('euler-theme', theme);
-    }
-  }
-});
 </script>
 
 <template>
   <div class="theme-box">
-    <div v-if="device" class="theme-box-pc" @click="changeTheme">
+    <div v-if="device" class="theme-box-pc">
       <OIcon class="icon">
         <IconMoonDark v-if="isLight" />
         <IconMoonLight v-else />
@@ -76,7 +35,6 @@ onMounted(() => {
         active-text="dark"
         inactive-text="light"
         active-color="#00685a"
-        @click.stop="changeTheme"
       />
     </div>
   </div>
