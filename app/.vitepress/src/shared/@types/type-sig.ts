9ba3d56befb2a94e000719e1b5afa41c7f72dc63
pxq <1456889284@qq.com>
2023-06-08 10:08:08 +0800

    fix:处理sig详情页面两个楼层间新增多个楼层场景

--- app/.vitepress/src/shared/@types/type-sig.ts
+++ app/.vitepress/src/shared/@types/type-sig.ts
@@ -7,3 +7,24 @@ export interface GroupInfo {
   groupName: string;
   features: FeatureInfo[];
 }
+
+export interface SigList {
+  group_name: string;
+  maillist: string;
+}
+
+export interface InfoItem {
+  content: string;
+  content_type: string;
+  description: string;
+  name: string;
+  title: string;
+}
+export interface EasyeditorInfoDataItem {
+  content: string;
+  content_type: string;
+  description: string;
+  name: string;
+  title: string;
+  items?: InfoItem[];
+}
