5a5f276c8efe99f989c5fbe4e312a187abab5341
haml-707 <845831435@qq.com>
2023-07-25 09:23:34 +0800

    fix:技术白皮书链接错误

--- app/.vitepress/src/data/showcase/technical-while-paper.ts
+++ app/.vitepress/src/data/showcase/technical-while-paper.ts
@@ -11,7 +11,7 @@ export default [
   },
   {
     banner: '',
-    path: '/whitepaper/openEuler%2022.03%20LTS%20SP2技术白皮书.pdf',
+    path: '/whitepaper/openEuler%2022.03%20LTS%20SP2%20技术白皮书.pdf',
     summary: 'openEuler 22.03 LTS SP2 白皮书',
   },
 ];
