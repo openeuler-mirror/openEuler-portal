6ca98edac6d18450747c3f15ca9fea69da848c45
haml-707 <845831435@qq.com>
2023-08-08 17:52:49 +0800

    feat:新增 22.03_LTS_SP2 英文 白皮书

--- app/.vitepress/src/data/home/index.ts
+++ app/.vitepress/src/data/home/index.ts
@@ -1,15 +1,13 @@
 import banner1_pc from '@/assets/category/home/banner/banner1_pc.jpg';
 import banner1_mo from '@/assets/category/home/banner/banner1_mo.jpg';
+
 import banner2_pc from '@/assets/category/home/banner/banner2_pc.jpg';
 import banner2_mo from '@/assets/category/home/banner/banner2_mo.jpg';
 import eulerSky_pc from '@/assets/category/home/banner/euler-sky_pc.jpg';
-import eulerSky_mo from '@/assets/category/home/banner/euler-sky_mo.png';
+import eulerSky_mo from '@/assets/category/home/banner/euler-sky_mo.jpg';
 
 import kunpeng_pc from '@/assets/category/home/banner/kunpeng_pc.jpg';
 import kunpeng_mo from '@/assets/category/home/banner/kunpeng_mo.jpg';
-
-import easyEdit_pc from '@/assets/category/home/banner/easy-edit_pc.jpg';
-import easyEdit_mo from '@/assets/category/home/banner/easy-edit_mo.jpg';
 
 import techDay_pc from '@/assets/category/home/banner/tech-day_pc.png';
 import techDay_mo from '@/assets/category/home/banner/tech-day_mo.png';
@@ -43,7 +41,7 @@ export default {
         pcBanner: eulerSky_pc,
         moBanner: eulerSky_mo,
         link: '/zh/community/program/',
-        id: 'euler-sky',
+        id: '',
         title: ['openEuler Call for X 计划'],
         desc: ['热衷实践 乐于分享 多元贡献'],
         btn: '查看详情',
@@ -54,16 +52,6 @@ export default {
         moBanner: kunpeng_mo,
         link: 'https://www.hikunpeng.com/developer/contests/kunpeng-competition2023?tab=4',
         id: '',
-        targetTap: 1,
-      },
-      {
-        pcBanner: easyEdit_pc,
-        moBanner: easyEdit_mo,
-        link: '/zh/blog/20230506-EasyEditor/20230506-EasyEditor.html',
-        title: ['社区SIG主页支持自编辑'],
-        desc: ['欢迎使用EasyEditor'],
-        btn: '查看详情',
-        id: 'easy-edit',
         targetTap: 1,
       },
       {
