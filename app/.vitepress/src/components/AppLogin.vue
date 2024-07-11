be4ccd22e1e391cfb41f1d3a777e73f34507e558
lishengbo <1171734220@qq.com>
2024-07-11 23:30:50 +0800

    fix: 修改个人中心下拉框hover文字颜色

--- app/.vitepress/src/components/AppLogin.vue
+++ app/.vitepress/src/components/AppLogin.vue
@@ -110,7 +110,7 @@ const jumpToUserZone = () => {
 
       &:hover {
         background: var(--o-color-brand1);
-        // color: var(--o-color-text2);
+        color: var(--o-color-white);
       }
       &.active {
         color: var(--o-color-brand1);
