871fd8fe1121448192b62f651ae848c0290905c9
邦邦邦邦 <15622356989@163.com>
2024-08-05 21:31:47 +0800

    docs: 完善wiki内容

--- app/.vitepress/src/data/wiki/wiki-toc.ts
+++ app/.vitepress/src/data/wiki/wiki-toc.ts
@@ -6,15 +6,15 @@ export default {
       children: [
         {
           label: '介绍',
-          link: 'introduce',
+          link: 'about/introduce',
         },
         {
           label: '快速链接',
-          link: 'quick-start',
+          link: 'about/quick-start',
         },
         {
-          label: '安装使用',
-          link: 'install',
+          label: '许可证',
+          link: 'about/licenses',
         },
       ],
     },
@@ -25,6 +25,32 @@ export default {
     {
       label: '安装使用',
       link: 'install-and-use',
+      children: [
+        {
+          label: '1. 公有云上 openEuler 镜像使用指南',
+          link: 'install-and-use/1',
+        },
+        {
+          label: '2. openEuler 容器镜像部署指南',
+          link: 'install-and-use/2',
+        },
+        {
+          label: '3. 如何在openEuler WSL中体验完整的桌面环境',
+          link: 'install-and-use/3',
+        },
+        {
+          label: '4. windows 下使用 VirtualBox 安装 openEuler',
+          link: 'install-and-use/4',
+        },
+        {
+          label: '5. Azure 获取 openEuler 共享镜像',
+          link: 'install-and-use/5',
+        },
+        {
+          label: '6. 在阿里云获取 openEuler 镜像',
+          link: 'install-and-use/6',
+        },
+      ],
     },
   ],
 };
--- app/zh/wiki/about/licenses/index.md
+++ app/zh/wiki/about/licenses/index.md
@@ -3,6 +3,6 @@ title: 关于 openEuler
 category: wiki
 ---
 
-## 许可证
+# 许可证
 
 openEuler 遵循 木兰宽松许可证第 2 版（MulanPSL2）
