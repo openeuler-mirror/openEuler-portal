8bab293d812cc97b8d410116bdb831cc44aa5d98
haml-707 <845831435@qq.com>
2024-11-10 22:48:29 +0800

    feat: 新增嘉宾楼层

--- app/.vitepress/src/views/summit/summit2024/@type/index.ts
+++ app/.vitepress/src/views/summit/summit2024/@type/index.ts
@@ -28,3 +28,42 @@ export interface ScheduleItemT {
     content: SectionT[];
   };
 }
+// -----------------------------
+// guest 数据类型
+
+export type GuestInfoT = {
+  type: string;
+  name: string;
+  title: string;
+  titleStripe: string;
+  guestClass: GuestClassT[];
+  datas: StoreDataT[];
+};
+
+export type TableDataT = {
+  id: string;
+  name: string;
+  nameVisible?: boolean;
+  img: string;
+  imgVisible?: boolean;
+  title: string;
+  titleVisible?: boolean;
+};
+
+export type GuestFormT = {
+  title: string;
+  titleStripe: string;
+  guestClass: GuestClassT[];
+};
+
+export type GuestClassT = {
+  title: string;
+  isActive: boolean;
+  id: string;
+};
+
+// 本地存储的数据类型
+export type StoreDataT = {
+  id: string;
+  data: TableDataT[];
+};
