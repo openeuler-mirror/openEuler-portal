6642f82cbdb8c904865a38f69e2f24e3016a9ce4
HuangSJ <huangpersimmonzzz@gmail.com>
2025-04-28 20:11:44 +0800

    update: 城市用户组配置文件

---
+++ app/.vitepress/src-new/data/user-group/city/zhenzhou.ts
@@ -0,0 +1,48 @@
+import 苏伟锋 from '~@/assets/category/user-group/zhenzhou/苏伟锋.png';
+import 吴淑跃 from '~@/assets/category/user-group/zhenzhou/吴淑跃.png';
+import 苏伟锋Dark from '~@/assets/category/user-group/zhenzhou/苏伟锋-dark.png';
+import 吴淑跃Dark from '~@/assets/category/user-group/zhenzhou/吴淑跃-dark.png';
+
+export default {
+  title: '郑州用户组',
+  organizational: '超聚变数字技术有限公司',
+  organizer: [
+    {
+      name: '苏伟锋',
+      position: '超聚变数字技术有限公司',
+      technology: [],
+      homePage: '',
+      email: 'suweifeng@xfusion.com',
+      avatar: 苏伟锋,
+      avatarDark: 苏伟锋Dark,
+      contribution: '',
+    },
+  ],
+  ambassador: [
+    {
+      name: '吴淑跃',
+      position: 'N/A',
+      technology: ['嵌入式'],
+      homePage: '',
+      avatar: 吴淑跃,
+      avatarDark: 吴淑跃Dark,
+    },
+  ],
+  news: [
+    {
+      date: '2023-10-31',
+      summary: '10月27日，openEuler社区与超聚变联合举办郑州用户组线下Meetup。',
+      archives: '2023-10',
+      articleName: '20231103-zhengzhou.md',
+      author: ['openEuler'],
+      banner: 'img/banners/20231102-zz.png',
+      type: 'news',
+      title:
+        'openEuler郑州用户组成立！openEuler与超聚变携手建设河南区域用户生态',
+      tags: ['openEuler'],
+      path: 'zh/news/20231103-zhengzhou/20231103-zhengzhou',
+      lang: 'zh',
+      category: 'news',
+    },
+  ],
+};
